951875b35435048c80db7ac2104a56bdde755888
hilings2 <hangzhang.h@gmail.com>
2026-06-09 17:56:18 -0700

    [c#] 3633. Earliest Finish Time for Land and Water Rides I

---
+++ cs/3600-3699/3633/3633. Earliest Finish Time for Land and Water Rides I.cs
@@ -0,0 +1,63 @@
+using System.Diagnostics;
+
+public class Solution {
+    public int EarliestFinishTime0(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int n = landStartTime.Length, m = waterStartTime.Length;
+        int res = int.MaxValue;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                int landThenWater = Math.Max(landStartTime[i] + landDuration[i], waterStartTime[j]) + waterDuration[j];
+                res = Math.Min(res, landThenWater);
+                int waterThenLand = Math.Max(waterStartTime[j] + waterDuration[j], landStartTime[i]) + landDuration[i];
+                res = Math.Min(res, waterThenLand);
+            }
+        }
+        return res;
+    }
+    public int EarliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
+        int n = landStartTime.Length, m = waterStartTime.Length;
+        int res = int.MaxValue;
+        int minEndTime = int.MaxValue;
+        for (int i = 0; i < n; i++)
+        {
+            minEndTime = Math.Min(minEndTime, landStartTime[i] + landDuration[i]);
+        }
+        for (int j = 0; j < m; j++)
+        {
+            res = Math.Min(res, Math.Max(minEndTime, waterStartTime[j]) + waterDuration[j]);
+        }
+        
+        minEndTime = int.MaxValue;
+        for (int j = 0; j < m; j++)
+        {
+            minEndTime = Math.Min(minEndTime, waterStartTime[j] + waterDuration[j]);
+        }
+        for (int i = 0; i < n; i++)
+        {
+            res = Math.Min(res, Math.Max(minEndTime, landStartTime[i]) + landDuration[i]);
+        }
+        return res;
+    }
+}
+
+class Program {
+    static void Main(string[] args) {
+        Solution sol = new();
+
+        int[] landStartTime = [2, 8];
+        int[] landDuration = [4, 1];
+        int[] waterStartTime = [6];
+        int[] waterDuration = [3];
+        Debug.Assert(sol.EarliestFinishTime(landStartTime, landDuration, waterStartTime, waterDuration) == 9);
+
+        landStartTime = [5];
+        landDuration = [3];
+        waterStartTime = [1];
+        waterDuration = [10];
+        Debug.Assert(sol.EarliestFinishTime(landStartTime, landDuration, waterStartTime, waterDuration) == 14);
+
+        Console.WriteLine("passed");
+    }
+}
